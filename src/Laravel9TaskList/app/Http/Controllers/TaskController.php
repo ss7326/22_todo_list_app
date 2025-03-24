0dedfb48a6d5847c6f655e228e48fb6372dbdc52
a7uku <htky3131@gmail.com>
2025-03-24 23:17:11 +0900

    add task controller

---
+++ src/Laravel9TaskList/app/Http/Controllers/TaskController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    // index method
+    public function index()
+    {
+        return "Hello world";
+    }
+}
