7bd8b0347b70fdea9bd732f4a85773772ba3ee61
a7uku <htky3131@gmail.com>
2025-04-04 22:29:22 +0900

    display folder create template

--- src/Laravel9TaskList/app/Http/Controllers/FolderController.php
+++ src/Laravel9TaskList/app/Http/Controllers/FolderController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class FolderController extends Controller
 {
-    //
+    /**
+     *  【フォルダ作成ページの表示機能】
+     *
+     *  GET /folders/create
+     *  @return \Illuminate\View\View
+     */
+    public function showCreateForm()
+    {
+        return view('folders/create');
+    }
 }
