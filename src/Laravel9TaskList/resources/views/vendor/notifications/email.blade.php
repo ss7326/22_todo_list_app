5db86cf11fb3219c2fdc25be10b4747f78ebad86
a7uku <htky3131@gmail.com>
2025-04-27 22:11:58 +0900

    err, mail layout

--- src/Laravel9TaskList/resources/views/vendor/notifications/email.blade.php
+++ src/Laravel9TaskList/resources/views/vendor/notifications/email.blade.php
@@ -1,58 +1,63 @@
 <x-mail::message>
-{{-- Greeting --}}
-@if (! empty($greeting))
-# {{ $greeting }}
-@else
-@if ($level === 'error')
-# @lang('Whoops!')
-@else
-# @lang('Hello!')
-@endif
-@endif
+    {{-- Greeting --}}
+    @if (! empty($greeting))
+    # {{ $greeting }}
+    @else
+    @if ($level === 'error')
+    # @lang('Whoops!')
+    @else
+    # @lang('Hello!')
+    @endif
+    @endif
 
-{{-- Intro Lines --}}
-@foreach ($introLines as $line)
-{{ $line }}
+    {{-- Intro Lines --}}
+    @foreach ($introLines as $line)
+    {{ $line }}
 
-@endforeach
+    @endforeach
 
-{{-- Action Button --}}
-@isset($actionText)
-<?php
+    {{-- Action Button --}}
+    @isset($actionText)
+    <?php
     $color = match ($level) {
         'success', 'error' => $level,
         default => 'primary',
     };
-?>
-<x-mail::button :url="$actionUrl" :color="$color">
-{{ $actionText }}
-</x-mail::button>
-@endisset
+    ?>
+    <x-mail::button :url="$actionUrl" :color="$color">
+        {{ $actionText }}
+    </x-mail::button>
+    @endisset
 
-{{-- Outro Lines --}}
-@foreach ($outroLines as $line)
-{{ $line }}
+    {{-- Outro Lines --}}
+    @foreach ($outroLines as $line)
+    {{ $line }}
 
-@endforeach
+    @endforeach
 
-{{-- Salutation --}}
-@if (! empty($salutation))
-{{ $salutation }}
-@else
-@lang('Regards'),<br>
-{{ config('app.name') }}
-@endif
+    {{-- Salutation --}}
+    @if (! empty($salutation))
+    {{ $salutation }}A
+    @else
+    @lang('Regards')<br><br>
+    <p>============================================
+        <br>
+        開発者 : ToDOApp開発チーム<br>
+        提供元 : {{ config('app.name') }}
+        ============================================
+    </p>
+    @endif
 
-{{-- Subcopy --}}
-@isset($actionText)
-<x-slot:subcopy>
-@lang(
-    "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser:',
-    [
+    {{-- Subcopy --}}
+    @isset($actionText)
+    <x-slot:subcopy>
+        @lang(
+        "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
+        'into your web browser:',
+        [
         'actionText' => $actionText,
-    ]
-) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
-</x-slot:subcopy>
-@endisset
+        ]
+        ) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
+    </x-slot:subcopy>
+    @endisset
 </x-mail::message>
