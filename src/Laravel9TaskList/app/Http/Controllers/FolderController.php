ea1cc642d611c05ee67169f7f79a5b2f1ac381e6
ss7326 <htky3131@gmail.com>
2025-05-05 14:24:22 +0900

    done, chpt 17
    Squashed commit of the following:
    
    commit c9ac986138ed3134bd052b5d038f61d4a91a2e0f
    Author: a7uku <htky3131@gmail.com>
    Date:   Mon May 5 14:23:54 2025 +0900
    
        success to catch exception, and log message
    
    commit a8c0e7079de7106f317251c31f97e6786634076e
    Author: a7uku <htky3131@gmail.com>
    Date:   Mon May 5 14:17:16 2025 +0900
    
        impl, transaction for delete, and try...catch
    
    commit 13a9821d44277277839e5ad1d199bceab56ccc98
    Author: a7uku <htky3131@gmail.com>
    Date:   Mon May 5 14:14:42 2025 +0900
    
        impl, try..catch
    
    commit 4e7859bf37c14e6caea5c38769468e54b0856156
    Author: a7uku <htky3131@gmail.com>
    Date:   Mon May 5 14:13:09 2025 +0900
    
        just sort methods, index, create, edit, delete
    
    commit 7ad3461388e1447e32a07a54a1ee95ec9d99548d
    Author: a7uku <htky3131@gmail.com>
    Date:   Mon May 5 14:00:39 2025 +0900
    
        just sort method, create, edit, delete, and display and actual process
    
    commit 59d99daa04dc388dd19500dac968bf346d17e24f
    Author: a7uku <htky3131@gmail.com>
    Date:   Sun May 4 17:44:19 2025 +0900
    
        _progress
    
    commit af3a67b3885d2377f1bdfeb4abb45f96f5ab6e01
    Author: a7uku <htky3131@gmail.com>
    Date:   Sun May 4 17:41:34 2025 +0900
    
        write error page, and abort code for test
    
    commit 0f27ef1122426aac2a3db6af9307100f985a9702
    Author: a7uku <htky3131@gmail.com>
    Date:   Sun May 4 17:32:53 2025 +0900
    
        init error pages
    
    commit 713aa46cc747544be73a69196cc866c14bc8a39f
    Author: a7uku <htky3131@gmail.com>
    Date:   Sun May 4 17:27:01 2025 +0900
    
        _progress

--- src/Laravel9TaskList/app/Http/Controllers/FolderController.php
+++ src/Laravel9TaskList/app/Http/Controllers/FolderController.php
@@ -2,94 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
-use App\Http\Requests\EditFolder;
-use App\Http\Requests\CreateFolder;
-use App\Models\Folder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use App\Models\Folder;
+use App\Http\Requests\CreateFolder;
+use App\Http\Requests\EditFolder;
 
 class FolderController extends Controller
 {
     /**
-     *  【フォルダの削除機能】
+     *  【フォルダ作成ページの表示機能】
      *
-     *  POST /folders/{folder}/delete
-     *  @param Folder $folder
-     *  @return RedirectResponse
-     */
-    public function delete(Folder $folder)
-    {
-        /** @var App\Models\User **/
-        $user = Auth::user();
-        $folder = $user->folders()->findOrFail($folder->id);
-
-        $folder->tasks()->delete();
-        $folder->delete();
-
-        $folder = Folder::first();
-
-        return redirect()->route('tasks.index', [
-            'folder' => $folder->id
-        ]);
-    }
-
-    /**
-     *  【フォルダ削除ページの表示機能】
-     *
-     *  GET /folders/{folder}/delete
-     *  @param Folder $folder
+     *  GET /folders/create
      *  @return \Illuminate\View\View
      */
-    public function showDeleteForm(Folder $folder)
+    public function showCreateForm()
     {
-        /** @var App\Models\User **/
-        $user = Auth::user();
-        $folder = $user->folders()->findOrFail($folder->id);
+        try {
+            /** @var App\Models\User **/
+            $user = Auth::user();
+            $user->folders;
 
-        return view('folders/delete', [
-            'folder_id' => $folder->id,
-            'folder_title' => $folder->title,
-        ]);
-    }
-
-    /**
-     *  【フォルダの編集機能】
-     *
-     *  POST /folders/{folder}/edit
-     *  @param Folder $folder
-     *  @param EditTask $request
-     *  @return \Illuminate\Http\RedirectResponse
-     */
-    public function edit(Folder $folder, EditFolder $request)
-    {
-        /** @var App\Models\User **/
-        $user = Auth::user();
-        $folder = $user->folders()->findOrFail($folder->id);
-        $folder->title = $request->title;
-        $folder->save();
-
-        return redirect()->route('tasks.index', [
-            'folder' => $folder->id,
-        ]);
-    }
-
-    /**
-     *  【フォルダ編集ページの表示機能】
-     *
-     *  GET /folders/{folder}/edit
-     *  @param Folder $folder
-     *  @return \Illuminate\View\View
-     */
-    public function showEditForm(Folder $folder)
-    {
-        /** @var App\Models\User **/
-        $user = Auth::user();
-        $folder = $user->folders()->findOrFail($folder->id);
-
-        return view('folders/edit', [
-            'folder_id' => $folder->id,
-            'folder_title' => $folder->title,
-        ]);
+            return view('folders/create');
+        } catch (\Throwable $e) {
+            Log::error('Error FolderController in showCreateForm: ' . $e->getMessage());
+        }
     }
 
     /**
@@ -102,35 +41,121 @@ class FolderController extends Controller
      */
     public function create(CreateFolder $request)
     {
+        try {
+            $folder = new Folder();
+            $folder->title = $request->title;
 
-        $folder = new Folder();
-        $folder->title = $request->title;
+            /** @var App\Models\User **/
+            $user = Auth::user();
+            $user->folders()->save($folder);
 
-        /** @var App\Models\User **/
-        $user = Auth::user();
-        $user->folders()->save($folder);
-
-        return redirect()->route('tasks.index', [
-            'folder' => $folder->id,
-        ]);
+            return redirect()->route('tasks.index', [
+                'folder' => $folder->id,
+            ]);
+        } catch (\Exception $e) {
+            Log::error('Error FolderController in create: ' . $e->getMessage());
+        }
     }
 
     /**
-     *  【フォルダ作成ページの表示機能】
+     *  【フォルダ編集ページの表示機能】
      *
-     *  GET /folders/create
+     *  GET /folders/{folder}/edit
+     *  @param Folder $folder
      *  @return \Illuminate\View\View
      */
-    public function showCreateForm()
+    public function showEditForm(Folder $folder)
     {
-        // ログインユーザーに紐づくフォルダだけを取得
-        /** @var App\Models\User **/
-        $user = Auth::user();
-        $user->folders;
+        try {
+            /** @var App\Models\User **/
+            $user = Auth::user();
+            $folder = $user->folders()->findOrFail($folder->id);
 
-        return view('folders/create');
-        // ?
-        // chpt.12, ユーザーに紐づけてフォルダを作成する
-        // return view('folders/create', compact('folders'));
+            return view('folders/edit', [
+                'folder_id' => $folder->id,
+                'folder_title' => $folder->title,
+            ]);
+        } catch (\Throwable $e) {
+            Log::error('Error FolderController in showEditForm: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     *  【フォルダの編集機能】
+     *
+     *  POST /folders/{folder}/edit
+     *  @param Folder $folder
+     *  @param EditTask $request
+     *  @return \Illuminate\Http\RedirectResponse
+     */
+    public function edit(Folder $folder, EditFolder $request)
+    {
+        try {
+            /** @var App\Models\User **/
+            $user = Auth::user();
+            $folder = $user->folders()->findOrFail($folder->id);
+            $folder->title = $request->title;
+            $folder->save();
+
+            return redirect()->route('tasks.index', [
+                'folder' => $folder->id,
+            ]);
+        } catch (\Throwable $e) {
+            Log::error('Error FolderController in edit: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     *  【フォルダ削除ページの表示機能】
+     *
+     *  GET /folders/{folder}/delete
+     *  @param Folder $folder
+     *  @return \Illuminate\View\View
+     */
+    public function showDeleteForm(Folder $folder)
+    {
+        try {
+            /** @var App\Models\User **/
+            $user = Auth::user();
+            $folder = $user->folders()->findOrFail($folder->id);
+
+            return view('folders/delete', [
+                'folder_id' => $folder->id,
+                'folder_title' => $folder->title,
+            ]);
+        } catch (\Throwable $e) {
+            Log::error('Error in showDeleteForm: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     *  【フォルダの削除機能】
+     *
+     *  POST /folders/{folder}/delete
+     *  @param Folder $folder
+     *  @return RedirectResponse
+     */
+    public function delete(Folder $folder)
+    {
+        try {
+            /** @var App\Models\User **/
+            $user = Auth::user();
+            $folder = $user->folders()->findOrFail($folder->id);
+
+            $folder = DB::transaction(function () use ($folder) {
+                if ($folder) throw new \Exception('500');
+                $folder->tasks()->delete();
+                $folder->delete();
+                return $folder;
+            });
+
+            $folder = Folder::first();
+
+            return redirect()->route('tasks.index', [
+                'folder' => $folder->id
+            ]);
+        } catch (\Throwable $e) {
+            Log::error('Error FolderController in delete: ' . $e->getMessage());
+        }
     }
 }
